8104b6417cdf24a73dbf63d13833f73f5374e5cb
Misha Kav <misha.kav@gmail.com>
2019-07-03 23:13:38 +0300

    add storybook-static

---
+++ storybook-static/main.be4f5271e48cd7afff4c.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(438),p(541),n.exports=p(928)},541:function(n,o,p){"use strict";p.r(o);p(542),p(917)}},[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.d872aa564c4d9476d9dbd244e31c9739.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24695d20f24c0f956eb285bb05b3bb26",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.122272e513ee3aa32500.bundle.js"
+  },
+  {
+    "url": "runtime~main.122272e513ee3aa32500.bundle.js"
+  },
+  {
+    "url": "vendors~main.122272e513ee3aa32500.bundle.js"
+  },
+  {
+    "revision": "58e8de1fc3b545d0dbad",
+    "url": "vendors~main.css"
+  }
+]);
